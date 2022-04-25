b04241861072de75cf6c438a5df85124ce4b4cbf
nick <nick@babblevoice.com>
2022-04-25 15:39:16 +0100

    a bit more coplexity

--- test/interface/index.js
+++ test/interface/index.js
@@ -7,7 +7,8 @@ const park = require( "../../index.js" )
 describe( "interface index.js", function() {
   it( `park emit`, async function() {
     let call = {
-      "vars": {}
+      "vars": {},
+      "channels": { "audio": false }
     }
 
     let parklot = park.create()
@@ -17,19 +18,21 @@ describe( "interface index.js", function() {
     } )
 
 
-    expect( parklot.park( call, "parkinglot" ) ).to.equal( "01" )
-    expect( call.vars.parking ).to.equal( "01" )
+    expect( parklot.park( call, "parkinglot" ).vars.parking.slot ).to.equal( "01" )
+    expect( call.vars.parking.slot ).to.equal( "01" )
     expect( parkcount ).to.equal( 1 )
 
   } )
 
   it( `park multiple emit`, async function() {
     let call = {
-      "vars": {}
+      "vars": {},
+      "channels": { "audio": false }
     }
 
     let othercall = {
-      "vars": {}
+      "vars": {},
+      "channels": { "audio": false }
     }
 
     let parklot = park.create()
@@ -39,11 +42,11 @@ describe( "interface index.js", function() {
     } )
 
 
-    expect( parklot.park( call, "parkinglot" ) ).to.equal( "01" )
-    expect( call.vars.parking ).to.equal( "01" )
+    expect( parklot.park( call, "parkinglot" ).vars.parking.slot ).to.equal( "01" )
+    expect( call.vars.parking.slot ).to.equal( "01" )
 
-    expect( parklot.park( othercall, "parkinglot" ) ).to.equal( "02" )
-    expect( othercall.vars.parking ).to.equal( "02" )
+    expect( parklot.park( othercall, "parkinglot" ).vars.parking.slot ).to.equal( "02" )
+    expect( othercall.vars.parking.slot ).to.equal( "02" )
 
     expect( parkcount ).to.equal( 2 )
 
@@ -51,11 +54,13 @@ describe( "interface index.js", function() {
 
   it( `park multiple and unpark`, async function() {
     let call = {
-      "vars": {}
+      "vars": {},
+      "channels": { "audio": false }
     }
 
     let othercall = {
-      "vars": {}
+      "vars": {},
+      "channels": { "audio": false }
     }
 
     let parklot = park.create()
@@ -69,33 +74,35 @@ describe( "interface index.js", function() {
     } )
 
 
-    expect( parklot.park( call, "parkinglot" ) ).to.equal( "01" )
-    expect( call.vars.parking ).to.equal( "01" )
+    expect( parklot.park( call, "parkinglot" ).vars.parking.slot ).to.equal( "01" )
+    expect( call.vars.parking.slot ).to.equal( "01" )
 
-    expect( parklot.park( othercall, "parkinglot" ) ).to.equal( "02" )
-    expect( othercall.vars.parking ).to.equal( "02" )
+    expect( parklot.park( othercall, "parkinglot" ).vars.parking.slot ).to.equal( "02" )
+    expect( othercall.vars.parking.slot ).to.equal( "02" )
 
     expect( parkcount ).to.equal( 2 )
 
-    expect( parklot.unpark( "parkinglot" ).vars.parking ).to.equal( "01" )
-    expect( parklot.unpark( "parkinglot" ).vars.parking ).to.equal( "02" )
+    expect( parklot.unpark( "parkinglot" ).vars.parking.slot ).to.equal( "01" )
+    expect( parklot.unpark( "parkinglot" ).vars.parking.slot ).to.equal( "02" )
 
     expect( parkcount ).to.equal( 0 )
 
-    expect( parklot.park( call, "parkinglot" ) ).to.equal( "01" )
-    expect( parklot.park( call, "parkinglot" ) ).to.equal( "02" )
-    expect( call.vars.parking ).to.equal( "02" )
-    expect( parklot.unpark( "parkinglot", "02" ).vars.parking ).to.equal( "02" )
+    expect( parklot.park( call, "parkinglot" ).vars.parking.slot ).to.equal( "01" )
+    expect( parklot.park( call, "parkinglot" ).vars.parking.slot ).to.equal( "02" )
+    expect( call.vars.parking.slot ).to.equal( "02" )
+    expect( parklot.unpark( "parkinglot", "02" ).vars.parking.slot ).to.equal( "02" )
 
   } )
 
   it( `park multiple lots`, async function() {
     let call = {
-      "vars": {}
+      "vars": {},
+      "channels": { "audio": false }
     }
 
     let othercall = {
-      "vars": {}
+      "vars": {},
+      "channels": { "audio": false }
     }
 
     let parklot = park.create()
@@ -108,10 +115,10 @@ describe( "interface index.js", function() {
       parkcount--
     } )
 
-    expect( parklot.park( call, "domainone.com" ) ).to.equal( "01" )
-    expect( call.vars.parking ).to.equal( "01" )
+    expect( parklot.park( call, "domainone.com" ).vars.parking.slot ).to.equal( "01" )
+    expect( call.vars.parking.slot ).to.equal( "01" )
 
-    expect( parklot.park( othercall, "domaintwo.com" ) ).to.equal( "01" )
-    expect( othercall.vars.parking ).to.equal( "01" )
+    expect( parklot.park( othercall, "domaintwo.com" ).vars.parking.slot ).to.equal( "01" )
+    expect( othercall.vars.parking.slot ).to.equal( "01" )
   } )
 } )
